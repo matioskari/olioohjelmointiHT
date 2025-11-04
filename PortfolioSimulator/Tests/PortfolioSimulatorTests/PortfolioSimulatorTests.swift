c959dd90b6a2b3c5a95d0221d397a12aeb7d300e
kallmovu <matiaskallio.oskari@gmail.com>
2025-11-04 13:02:59 +0200

    uusi kuva UML-kaaviosta

---
+++ PortfolioSimulator/Tests/PortfolioSimulatorTests/PortfolioSimulatorTests.swift
@@ -0,0 +1,9 @@
+// import XCTest
+@testable import PortfolioSimulator
+
+final class PortfolioSimulatorTests: XCTestCase {
+    func testExample() throws {
+        // Tämä on vain esimerkkitesti — voit muokata myöhemmin
+        XCTAssertTrue(1 + 1 == 2)
+    }
+}
